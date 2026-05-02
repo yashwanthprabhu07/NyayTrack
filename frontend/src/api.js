2173f88a514bc1659a8966c7e72eb7180e94683d
yashwanthprabhu07 <yashwanth.prabhu.s.121@kalvium.community>
2026-05-02 12:56:11 +0530

    Update API URL to production backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,13 +1,17 @@
-import axios from 'axios';
+/** @format */
+
+import axios from "axios";
 
 const API = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: "https://nyaytrack-backend.onrender.com/api",
 });
 
-export const uploadPDF = (formData) => API.post('/upload', formData);
-export const getActions = () => API.get('/actions');
-export const getAction = (id) => API.get('/actions/' + id);
-export const updateStatus = (id, status) => API.patch('/actions/' + id + '/status?status=' + status);
-export const searchCase = (caseNumber) => API.get('/actions/search/' + caseNumber);
+export const uploadPDF = (formData) => API.post("/upload", formData);
+export const getActions = () => API.get("/actions");
+export const getAction = (id) => API.get("/actions/" + id);
+export const updateStatus = (id, status) =>
+  API.patch("/actions/" + id + "/status?status=" + status);
+export const searchCase = (caseNumber) =>
+  API.get("/actions/search/" + caseNumber);
 
 export default API;
